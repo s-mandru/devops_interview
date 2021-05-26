b5730bb9607ab3917f73697b898653901c4f71d4
Patrick Viet <patrick.viet@coherehealth.com>
2021-05-26 11:34:18 -0400

    slow loop

--- handler.rb
+++ handler.rb
@@ -6,7 +6,11 @@ set :port, 8080
 set :bind, '0.0.0.0'
 
 get '/' do
-  "BASIC STUFF"
+  s = Time.new.to_f
+  while(Time.new.to_f - s < 0.2)
+    true
+  end
+  "BASIC SLOW STUFF"
 end
 
 get '/healthcheck' do
