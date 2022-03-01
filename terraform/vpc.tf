fe25aee5dc07483148e9b407ac110ae201c9ce0c
s-mandru <surya.mandru@outlook.com>
2022-03-01 17:50:17 -0500

    azs

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "cohere"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
